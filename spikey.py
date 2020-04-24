50aa6bc56ee348ff2a7ab6f919f8012d95e9a122
Jared Pineiro <jlvpineiro@gmail.com>
2020-04-24 15:40:25 -0400

    adding spaces

--- spikey.py
+++ spikey.py
@@ -2,7 +2,7 @@
 import keras
 import numpy
 from numpy import asarray
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.datasets import imdb
 from keras.layers import Dense, Flatten, Dropout, Activation, Reshape
 from keras.layers.convolutional import Conv1D, MaxPooling1D
@@ -12,16 +12,25 @@ from keras.utils.np_utils import to_categorical
 from sklearn.metrics import classification_report
 from data_sort import *
 from sklearn.model_selection import train_test_split
+import time
+
+start = time.time()
 
 numpy.random.seed(7)
 
-f1 = 'data/alphabet_04_10'
-f2 = 'data/alphabet_04_10_logkeys'
-f3 = 'data/alphabet_02_19'
-f4 = 'data/alphabet_02_19_logkeys'
+f1 = 'data/alphabet_02_19'
+f2 = 'data/alphabet_02_19_logkeys'
+f3 = 'data/alphabet_04_10'
+f4 = 'data/alphabet_04_10_logkeys'
+f5 = 'data/alphabet_4_10_2'
+f6 = 'data/alphabet_4_10_2_logkeys'
+f7 = 'data/alphabet_04_12'
+f8 = 'data/alphabet_04_12_logkeys'
+f9 = 'data/spaces_04_24'
+f10 = 'data/spaces_04_24_logkeys'
 
-acc_files = [f1]
-lk_files = [f2]
+acc_files = [f3,f5,f9]
+lk_files = [f4,f6,f10]
 
 # accData entries look like [String time, String x, String y, String z]
 acc_entry_list = make_AccEntry_List(acc_files)
@@ -30,10 +39,10 @@ acc_entry_list = make_AccEntry_List(acc_files)
 lk_entry_list = make_LKEntry_List(lk_files)
 
 # letters to look for
-checkLs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+checkLs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', '','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # how to classify the data
-classes = ['none', 'po', 'm', 'r', 'pi']
+classes = ['none', 'po', 'm', 'r', 'pi','space']
 
 windows = make_window_dict(checkLs, acc_entry_list, lk_entry_list)
 
@@ -41,7 +50,7 @@ add_non_keypress(windows,acc_files,split=True)
 
 time_output = []
 window_list = []
-fingers = {'po': ['r', 't', 'y', 'u', 'f', 'g', 'h,' 'j', 'v', 'b', 'n','m'], 'm': ['e', 'd', 'c','i','k'], 'r': ['w','s', 'x', 'o', 'l'], 'pi':['q','a','z','p']}
+fingers = {'po': ['r', 't', 'y', 'u', 'f', 'g', 'h,' 'j', 'v', 'b', 'n','m'], 'm': ['e', 'd', 'c','i','k'], 'r': ['w','s', 'x', 'o', 'l'], 'pi':['q','a','z','p'], 'space':['']}
 testing = 0
 for key in windows:
     for window in windows[key]:
@@ -57,6 +66,8 @@ for key in windows:
                 time_output.insert(i, (window.window[0].get_time(), 3))
             elif key in fingers.get('pi'):
                 time_output.insert(i, (window.window[0].get_time(), 4))
+            elif key in fingers.get('space'):
+                time_output.insert(i, (window.window[0].get_time(), 5))
             else:
                 time_output.insert(i, (window.window[0].get_time(), 0))
         else:
@@ -67,7 +78,7 @@ for key in windows:
         window_list.insert(i,cur_entry)
 
 output = [item[1] for item in time_output]
-print(output)
+# print(output)
 '''
 f_time = open("f_time_output", "w+")
 f_out = open("f_output", "w+")
@@ -113,36 +124,39 @@ y_test_hot = to_categorical(y_test,num_classes)
 print('New y_train shape: ', y_train_hot.shape)
 
 # create model
-embedding_vecor_length = 3
-model = Sequential()
+model = None
+# model = load_model('best_model.48-0.17.h5')
+if model is None:
+    embedding_vecor_length = 3
+    model = Sequential()
 
-model.add(Reshape((20,3), input_shape=(input_shape,)))
-model.add(Dense(300,activation='selu'))
-model.add(Flatten())
-model.add(Dense(num_classes,activation='softmax'))
-print(model.summary())
+    model.add(Reshape((20,3), input_shape=(input_shape,)))
+    model.add(Dense(300,activation='selu'))
+    model.add(Flatten())
+    model.add(Dense(num_classes,activation='softmax'))
+    print(model.summary())
 
-#model.add(Conv1D(filters=1, kernel_size=20, activation ='relu', input_shape=(x_train.shape[1],3)))
+    #model.add(Conv1D(filters=1, kernel_size=20, activation ='relu', input_shape=(x_train.shape[1],3)))
 
-callbacks_list = [
-    keras.callbacks.ModelCheckpoint(
-        filepath='best_model.{epoch:02d}-{val_loss:.2f}.h5',
-        monitor='val_loss', save_best_only=True),
-    keras.callbacks.EarlyStopping(monitor='acc', patience=1)
-]
+    callbacks_list = [
+        keras.callbacks.ModelCheckpoint(
+            filepath='best_model.{epoch:02d}-{val_loss:.2f}.h5',
+            monitor='val_loss', save_best_only=True),
+        keras.callbacks.EarlyStopping(monitor='acc', patience=1)
+    ]
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# print(model.summary())
-BATCH_SIZE = 20
-EPOCHS = 50
-history = model.fit(x_train, 
-        y_train_hot, 
-        batch_size=BATCH_SIZE,
-        epochs=EPOCHS,
-        callbacks=callbacks_list,
-        validation_split=.22,
-        verbose=1)
-        # validation_data=(x_test,y_test_hot))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # print(model.summary())
+    BATCH_SIZE = 20
+    EPOCHS = 50
+    history = model.fit(x_train, 
+            y_train_hot, 
+            batch_size=BATCH_SIZE,
+            epochs=EPOCHS,
+            callbacks=callbacks_list,
+            validation_split=.22,
+            verbose=1)
+            # validation_data=(x_test,y_test_hot))
 
 
 
@@ -182,6 +196,9 @@ mpo = 0
 mm = 0
 mr = 0
 mpi = 0
+space = 0
+wspace = 0
+mspace = 0
 
 
 for k in range(y_test.size):
@@ -205,6 +222,11 @@ for k in range(y_test.size):
             pi += 1
         else: 
             wpi += 1
+    if(max(y_pred_train[k]) == y_pred_train[k][5]):
+        if(y_test[k] == 5):
+            space += 1
+        else: 
+            wspace += 1
     else:
         if(y_test[k] == 0):
             none += 1
@@ -216,6 +238,8 @@ for k in range(y_test.size):
             mr += 1
         if(y_test[k] == 4):
             mpi += 1
+        if(y_test[k] == 4):
+            mspace += 1
     
 
 print("no finger correctly detected: ", none)
@@ -229,8 +253,14 @@ print("ring detected: ", r)
 print("false ring: ", wr)
 print("ring not detected: ", mr)
 print("pinky detected: ", pi)
-print("flase pi: ", wpi)
+print("false pi: ", wpi)
 print("pinky not detected: ", mpi)
+print("space: ", space)
+print("false space: ",wspace)
+print("space not detected: ", mspace)
+
+end = time.time()
+print(end - start)
     
 '''
 c1 = 0
