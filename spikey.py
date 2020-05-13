4b2a61aaf316b5089da68d15575345ddb7877687
Jared Pineiro <jlvpineiro@gmail.com>
2020-05-13 13:09:11 -0400

    dont' really remember

--- spikey.py
+++ spikey.py
@@ -18,19 +18,36 @@ start = time.time()
 
 numpy.random.seed(7)
 
-f1 = 'data/alphabet_02_19'
-f2 = 'data/alphabet_02_19_logkeys'
-f3 = 'data/alphabet_04_10'
-f4 = 'data/alphabet_04_10_logkeys'
-f5 = 'data/alphabet_4_10_2'
-f6 = 'data/alphabet_4_10_2_logkeys'
+f1 = 'data/alice_11_12'
+f2 = 'data/alice_11_12_logkeys'
+f3 = 'data/alphabet_02_19'
+f4 = 'data/alphabet_02_19_logkeys'
+f5 = 'data/alphabet_04_10'
+f6 = 'data/alphabet_04_10_logkeys'
 f7 = 'data/alphabet_04_12'
 f8 = 'data/alphabet_04_12_logkeys'
-f9 = 'data/spaces_04_24'
-f10 = 'data/spaces_04_24_logkeys'
+f9 = 'data/alphabet_11_12'
+f10 = 'data/alphabet_11_12_logkeys'
+f11 = 'data/alphabet_4_10_2'
+f12 = 'data/alphabet_4_10_2_logkeys'
+f13 = 'data/ethics_04_24'
+f14 = 'data/ethics_04_24_logkeys'
+f15 = 'data/ethics_04_24_2'
+f16 = 'data/ethics_04_24_2_logkeys'
+f17 = 'data/gatsby_11_12'
+f18 = 'data/gatsby_11_12_logkeys'
+f19 = 'data/gibberish_11_12'
+f20 = 'data/gibberish_11_12_logkeys'
+f21 = 'data/iliad_11_12'
+f22 = 'data/iliad_11_12_logkeys'
+f23 = 'data/spaces_04_24'
+f24 = 'data/spaces_04_24_logkeys'
 
-acc_files = [f3,f5,f9]
-lk_files = [f4,f6,f10]
+# acc_files = [f5,f11,f23]
+# lk_files = [f6,f12,f24]
+
+acc_files = [f1,f3,f5,f7,f9,f11,f17,f19,f21,f23]
+lk_files = [f2,f4,f6,f8,f10,f12,f18,f20,f22,f24]
 
 # accData entries look like [String time, String x, String y, String z]
 acc_entry_list = make_AccEntry_List(acc_files)
@@ -39,7 +56,7 @@ acc_entry_list = make_AccEntry_List(acc_files)
 lk_entry_list = make_LKEntry_List(lk_files)
 
 # letters to look for
-checkLs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', '','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+checkLs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', '','.','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # how to classify the data
 classes = ['none', 'po', 'm', 'r', 'pi','space']
@@ -50,7 +67,7 @@ add_non_keypress(windows,acc_files,split=True)
 
 time_output = []
 window_list = []
-fingers = {'po': ['r', 't', 'y', 'u', 'f', 'g', 'h,' 'j', 'v', 'b', 'n','m'], 'm': ['e', 'd', 'c','i','k'], 'r': ['w','s', 'x', 'o', 'l'], 'pi':['q','a','z','p'], 'space':['']}
+fingers = {'po': ['r', 't', 'y', 'u', 'f', 'g', 'h', 'j', 'v', 'b', 'n','m'], 'm': ['e', 'd', 'c','i','k'], 'r': ['w','s', 'x', 'o','.', 'l'], 'pi':['q','a','z','p'], 'space':['']}
 testing = 0
 for key in windows:
     for window in windows[key]:
@@ -125,44 +142,49 @@ print('New y_train shape: ', y_train_hot.shape)
 
 # create model
 model = None
-# model = load_model('best_model.48-0.17.h5')
+# model = load_model('new_model.h5')
 if model is None:
     embedding_vecor_length = 3
     model = Sequential()
 
-    model.add(Reshape((20,3), input_shape=(input_shape,)))
+    model.add(Reshape((default_range,3), input_shape=(input_shape,)))
     model.add(Dense(300,activation='selu'))
     model.add(Flatten())
     model.add(Dense(num_classes,activation='softmax'))
-    print(model.summary())
 
-    #model.add(Conv1D(filters=1, kernel_size=20, activation ='relu', input_shape=(x_train.shape[1],3)))
+    #model.add(Conv1D(filters=1, kernel_size=default_range, activation ='relu', input_shape=(x_train.shape[1],3)))
 
     callbacks_list = [
         keras.callbacks.ModelCheckpoint(
             filepath='best_model.{epoch:02d}-{val_loss:.2f}.h5',
             monitor='val_loss', save_best_only=True),
-        keras.callbacks.EarlyStopping(monitor='acc', patience=1)
+        keras.callbacks.EarlyStopping(monitor='accuracy', patience=1)
     ]
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     # print(model.summary())
-    BATCH_SIZE = 20
-    EPOCHS = 50
-    history = model.fit(x_train, 
-            y_train_hot, 
-            batch_size=BATCH_SIZE,
-            epochs=EPOCHS,
-            callbacks=callbacks_list,
-            validation_split=.22,
-            verbose=1)
-            # validation_data=(x_test,y_test_hot))
 
-
+# print(model.summary())
+# callbacks_list = [
+    # keras.callbacks.ModelCheckpoint(
+        # filepath='best_model.{epoch:02d}-{val_loss:.2f}.h5',
+        # monitor='val_loss', save_best_only=True),
+    # keras.callbacks.EarlyStopping(monitor='acc', patience=1)
+# ]
+# BATCH_SIZE = default_range
+# EPOCHS = 50
+# history = model.fit(x_train, 
+        # y_train_hot, 
+        # batch_size=BATCH_SIZE,
+        # epochs=EPOCHS,
+        # callbacks=callbacks_list,
+        # validation_split=.22,
+        # verbose=1)
+        # # validation_data=(x_test,y_test_hot))
 
 # plt.figure(figsize=(6, 4))
-# plt.plot(history.history['acc'], 'r', label='Accuracy of training data')
-# plt.plot(history.history['val_acc'], 'b', label='Accuracy of validation data')
+# plt.plot(history.history['accuracy'], 'r', label='Accuracy of training data')
+# plt.plot(history.history['val_accuracy'], 'b', label='Accuracy of validation data')
 # plt.plot(history.history['loss'], 'r--', label='Loss of training data')
 # plt.plot(history.history['val_loss'], 'b--', label='Loss of validation data')
 # plt.title('Model Accuracy and Loss')
@@ -183,6 +205,8 @@ print(classification_report(y_test, max_y_pred_train))
 
 
 none = 0
+wnone = 0
+mnone = 0
 po = 0
 wpo = 0
 m = 0
@@ -202,63 +226,78 @@ mspace = 0
 
 
 for k in range(y_test.size):
-    if(max(y_pred_train[k]) == y_pred_train[k][1]):
+    wrong = False
+    if(max(y_pred_train[k]) == y_pred_train[k][0]):
+        if(y_test[k] == 0):
+            none += 1
+        else:
+            wnone += 1
+            wrong = True
+    elif(max(y_pred_train[k]) == y_pred_train[k][1]):
         if(y_test[k] == 1):
             po += 1
         else:
             wpo += 1
-    if(max(y_pred_train[k]) == y_pred_train[k][2]):
+            wrong = True
+    elif(max(y_pred_train[k]) == y_pred_train[k][2]):
         if(y_test[k] == 2):
             m += 1
         else: 
             wm += 1
-    if(max(y_pred_train[k]) == y_pred_train[k][3]):
+            wrong = True
+    elif(max(y_pred_train[k]) == y_pred_train[k][3]):
         if(y_test[k] == 3):
             r += 1
         else: 
             wr += 1
-    if(max(y_pred_train[k]) == y_pred_train[k][4]):
+            wrong = True
+    elif(max(y_pred_train[k]) == y_pred_train[k][4]):
         if(y_test[k] == 4):
             pi += 1
         else: 
             wpi += 1
-    if(max(y_pred_train[k]) == y_pred_train[k][5]):
+            wrong = True
+    elif(max(y_pred_train[k]) == y_pred_train[k][5]):
         if(y_test[k] == 5):
             space += 1
         else: 
             wspace += 1
-    else:
+            wrong = True
+    if wrong:
         if(y_test[k] == 0):
-            none += 1
-        if(y_test[k] == 1):
+            mnone += 1
+        elif(y_test[k] == 1):
             mpo += 1
-        if(y_test[k] == 2):
+        elif(y_test[k] == 2):
             mm += 1
-        if(y_test[k] == 3):
+        elif(y_test[k] == 3):
             mr += 1
-        if(y_test[k] == 4):
+        elif(y_test[k] == 4):
             mpi += 1
-        if(y_test[k] == 4):
+        elif(y_test[k] == 4):
             mspace += 1
     
-
-print("no finger correctly detected: ", none)
-print("pointer correctly detected: ", po)
-print("false pointer: ", wpo)
-print("pointer not detected: ", mpo)
-print("m: ", m)
-print("false m: ",wm)
-print("middle not detected: ", mm)
-print("ring detected: ", r)
-print("false ring: ", wr)
-print("ring not detected: ", mr)
-print("pinky detected: ", pi)
-print("false pi: ", wpi)
-print("pinky not detected: ", mpi)
+print("no finger correct: ", none)
+print("no finger false: ", wnone)
+print("no finger missed: ", mnone)
+print("pointer correct: ", po)
+print("pointer false: ", wpo)
+print("pointer missed: ", mpo)
+print("middle correct: ", m)
+print("middle false: ",wm)
+print("middle missed: ", mm)
+print("ring correct: ", r)
+print("ring false: ", wr)
+print("ring missed: ", mr)
+print("pinky correct: ", pi)
+print("pinky false: ", wpi)
+print("pinky missed: ", mpi)
 print("space: ", space)
-print("false space: ",wspace)
-print("space not detected: ", mspace)
+print("space false: ",wspace)
+print("space missed: ", mspace)
 
+print(none + mnone + wnone + po + wpo + mpo + m + wm + mm + r + wr + mr + pi + wpi + mpi + space + wspace + mspace)
+print(none + wnone + po + wpo + m + wm + r + wr + pi + wpi + space + wspace)
 end = time.time()
 print(end - start)
     
